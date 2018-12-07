dad592b6ef277da83b108c104fce2090c4d5e72e
odiarra <g179914@rmm-p2100219pl.ads.local>
2018-12-07 16:25:03 +0100

    first commit

---
+++ tests/test_copy.py
@@ -0,0 +1,28 @@
+import pytest
+import datetime
+from jira_dest import *
+
+
+@pytest.fixture(autouse=True, scope="module")
+def jira_source_dest():
+    source_url, source_login, source_passwd = "http://localhost:8004", "admin", "admin"
+    dest_url, dest_login, dest_passwd = "http://localhost:8005", "admin", "admin"
+    sudo, verify = True, False
+
+
+    jira_source = jira(source_url,
+                              basic_auth=(source_login, source_passwd),
+                              max_retries=6,
+                              sudo=sudo,
+                              options={'verify': verify})
+    jira_dest = jira(dest_url,
+                            basic_auth=(dest_login, dest_passwd),
+                            max_retries=6,
+                            sudo=sudo,
+                            options={'verify': verify})
+
+    return jira_source, jira_dest
+
+
+
+
